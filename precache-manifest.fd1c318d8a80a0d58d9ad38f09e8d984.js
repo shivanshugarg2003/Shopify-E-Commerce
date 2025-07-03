db9d76eebd74da7698d8357d06c90021dfbd3853
Shivanshu Garg <shivanshugarg2003@gmail.com>
2025-07-03 14:05:45 +0530

    Updates

---
+++ precache-manifest.fd1c318d8a80a0d58d9ad38f09e8d984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934bf062a9d5d1dd021b050df16b2bdb",
+    "url": "/Shopify-E-Commerce/index.html"
+  },
+  {
+    "revision": "22b0b90044a7e165fc4e",
+    "url": "/Shopify-E-Commerce/static/css/main.228902f1.chunk.css"
+  },
+  {
+    "revision": "2a8fc4b3840f42f12026",
+    "url": "/Shopify-E-Commerce/static/js/2.f3ccb349.chunk.js"
+  },
+  {
+    "revision": "871e8585748db1cad8c9efc9ff3bbfad",
+    "url": "/Shopify-E-Commerce/static/js/2.f3ccb349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b0b90044a7e165fc4e",
+    "url": "/Shopify-E-Commerce/static/js/main.f28fabf1.chunk.js"
+  },
+  {
+    "revision": "e347d6d1eb90f6bf9b0b",
+    "url": "/Shopify-E-Commerce/static/js/runtime-main.227b42d6.js"
+  }
+]);
